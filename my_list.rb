66cc07c11ecce335f037a119cb2de17e0ee20284
touredri <dcompte223@gmail.com>
2023-06-15 20:30:43 +0100

    added MyList class

---
+++ my_list.rb
@@ -0,0 +1,24 @@
+require_relative 'my_enumerable'
+
+class MyList
+  include MyEnumerable
+
+  def initialize(*elements)
+    @list = elements
+  end
+
+  def each(&block)
+    @list.each(&block)
+  end
+end
+
+list = MyList.new(1, 2, 3, 4)
+
+puts
+puts ( list.all? { |e| e < 5 })
+puts (list.all? { |e| e > 5 })
+
+puts (list.any? { |e| e == 2 })
+puts (list.any? { |e| e == 5 })
+
+puts list.filter(&:even?)
